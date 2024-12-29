100a97d2633c3cf3dee89191effb6cfe362fba94
Sumitkrishna26 <sumitkrishna56@gmail.com>
2024-12-29 17:19:06 +0530

    Second commit change the numbers

--- src/javainterview/evenodd.java
+++ src/javainterview/evenodd.java
@@ -5,7 +5,7 @@ public class evenodd {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int[] a = {1,2,3,4,5};
+		int[] a = {1,2,3,4,5,6,7,8};
 		
 		 int even = 0, odd = 0;
 		 
